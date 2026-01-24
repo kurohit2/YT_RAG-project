f5fbca694b69fbf49bf42eb2a5e2626a7d77b76f
Rohit kumawat <rohitkumawat2746@gmail.com>
2026-01-24 11:17:57 +0530

    created full RAg based Youtbe project pythoin code

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os 
+from dotenv import load_dotenv
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 from langchain_text_splitters import RecursiveCharacterTextSplitter 
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough , RunnableParallel ,RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
 
 video_id = "QCX62YJCmGk"  # only the ID, not full URL
 
@@ -43,6 +46,41 @@ embeddings = HuggingFaceEmbeddings(
 
 # creating the vector store 
 vector_store = FAISS.from_documents(chunks, embeddings)
-query = "Why does sugar cause light to twist?"
-docs = vector_store.similarity_search(query)
-print(docs[0].page_content)
+
+# Reteriever 
+retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 4})
+
+llm = ChatGroq(
+    groq_api_key=os.environ.get("GROK_API_KEY"),
+    model_name="llama-3.1-8b-instant", 
+    temperature=0.2
+)
+
+#creating the promopt 
+prompt = PromptTemplate(
+    template="""
+      You are a helpful assistant.
+      Answer ONLY from the provided transcript context.
+      If the context is insufficient, just say you don't know.
+
+      {context}
+      Question: {question}
+    """,
+    input_variables = ['context', 'question']
+)
+
+def format_docs(retrieved_docs):
+    context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
+    return context_text
+
+#creating the chains 
+parallel_chain = RunnableParallel ({
+    'question': RunnablePassthrough(),
+    "context": retriever | RunnableLambda(format_docs)
+})
+parser = StrOutputParser()
+
+main_chain = parallel_chain | prompt | llm | parser
+
+response = main_chain.invoke("summarize into 100 words")
+print(response)
